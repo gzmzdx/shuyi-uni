048742309baf29d43a3bf6fa164615dca1caa112
wqm <1620194015@qq.com>
2020-11-25 12:44:30 +0800

    test(merge): 个人信息管理页面以及阅读圈子更多圈子部分页面得编写

---
+++ data.js
@@ -0,0 +1,159 @@
+export default [{
+		"name": "全部",
+		"foods": [{
+				"name": "三味书屋",
+				"key": "三味书屋",
+				"icon": "http://img.kiwifruits.cn/classify/1/2.jpg",
+				"cat": 10
+			},
+			{
+				"name": "时间很长",
+				"key": "时间很长",
+				"icon": "http://img.kiwifruits.cn/classify/1/2.jpg",
+				"cat": 10
+			},
+			{
+				"name": "三味书屋",
+				"key": "三味书屋",
+				"icon": "http://img.kiwifruits.cn/classify/1/3.jpg",
+				"cat": 10
+			},
+			{
+				"name": "时间很长",
+				"key": "时间很长",
+				"icon": "http://img.kiwifruits.cn/classify/1/2.jpg",
+				"cat": 10
+			},
+			{
+				"name": "时间很长",
+				"key": "时间很长",
+				"icon": "http://img.kiwifruits.cn/classify/1/2.jpg",
+				"cat": 10
+			},
+			{
+				"name": "时间很长",
+				"key": "时间很长",
+				"icon": "http://img.kiwifruits.cn/classify/1/2.jpg",
+				"cat": 10
+			},
+			{
+				"name": "时间很长",
+				"key": "时间很长",
+				"icon": "http://img.kiwifruits.cn/classify/1/2.jpg",
+				"cat": 10
+			},
+			
+			
+		{
+			"name": "时间很长",
+			"key": "时间很长",
+			"icon": "http://img.kiwifruits.cn/classify/1/2.jpg",
+			"cat": 10
+		},
+			
+			{
+				"name": "时间很长",
+				"key": "时间很长",
+				"icon": "http://img.kiwifruits.cn/classify/1/2.jpg",
+				"cat": 10
+			},
+				{
+					"name": "时间很长",
+					"key": "时间很长",
+					"icon": "http://img.kiwifruits.cn/classify/1/2.jpg",
+					"cat": 10
+				},
+					{
+						"name": "时间很长",
+						"key": "时间很长",
+						"icon": "http://img.kiwifruits.cn/classify/1/2.jpg",
+						"cat": 10
+					},
+						
+			
+			
+			{
+				"name": "时间很长",
+				"key": "时间很长",
+				"icon": "http://img.kiwifruits.cn/classify/1/2.jpg",
+				"cat": 10
+			}
+		]
+	},
+	{
+		"name": "我的",
+		"foods": [{
+				"name": "火锅",
+				"key": "火锅",
+				"icon": "http://img.kiwifruits.cn/classify/2/1.jpg",
+				"cat": 6
+			},
+			{
+				"name": "糕点饼干",
+				"key": "糕点饼干",
+				"icon": "http://img.kiwifruits.cn/classify/2/2.jpg",
+				"cat": 6
+			},
+			{
+				"name": "坚果果干",
+				"key": "坚果果干",
+				"icon": "http://img.kiwifruits.cn/classify/2/3.jpg",
+				"cat": 6
+			},
+			{
+				"name": "酒类",
+				"key": "酒类",
+				"icon": "http://img.kiwifruits.cn/classify/2/4.jpg",
+				"cat": 6
+			},
+			{
+				"name": "辣条",
+				"key": "辣条",
+				"icon": "http://img.kiwifruits.cn/classify/2/5.jpg",
+				"cat": 6
+			},
+			{
+				"name": "大礼包",
+				"key": "大礼包",
+				"icon": "http://img.kiwifruits.cn/classify/2/6.jpg",
+				"cat": 6
+			},
+			{
+				"name": "精品茗茶",
+				"key": "茶",
+				"icon": "http://img.kiwifruits.cn/classify/2/7.jpg",
+				"cat": 6
+			},
+			{
+				"name": "休闲食品",
+				"key": "休闲食品",
+				"icon": "http://img.kiwifruits.cn/classify/2/8.jpg",
+				"cat": 6
+			},
+			{
+				"name": "糖果巧克力",
+				"key": "糖果巧克力",
+				"icon": "http://img.kiwifruits.cn/classify/2/9.jpg",
+				"cat": 6
+			},
+			{
+				"name": "方便速食",
+				"key": "方便速食",
+				"icon": "http://img.kiwifruits.cn/classify/2/10.jpg",
+				"cat": 6
+			},
+			{
+				"name": "营养代餐",
+				"key": "营养代餐",
+				"icon": "http://img.kiwifruits.cn/classify/2/11.jpg",
+				"cat": 6
+			},
+			{
+				"name": "粮油副食",
+				"key": "粮油",
+				"icon": "http://img.kiwifruits.cn/classify/2/12.jpg",
+				"cat": 6
+			}
+		]
+	}
+]
--- pages/myinfomation/myinfomation.vue
+++ pages/myinfomation/myinfomation.vue
@@ -1,28 +1,57 @@
 <template>
-	<view>
+	<view class="all">
 		<view class="top">
 			<view class="topone">头像</view>
 			<view><image class="image" src="../../static/images/头像2.jpg" style="width:120rpx;height: 120rpx;border-radius: 100%;"></image></view>
-			<view class="topthree"><image class="imag" src="../../static/images/更多.png" style="width: 30rpx;height: 35rpx;"></image></view>
+			<!-- <view class="topthree"><image class="imag" src="../../static/images/更多.png" style="width: 30rpx;height: 35rpx;"></image></view> -->
 		</view>
+		<view class="br2"></view>
 		<view class="mid">
 			<view class="midone">昵称 :</view>
 			<view class="midone_r"><input placeholder="林思鱼" /></view>
 		</view>
+		<view class="br1"></view>
+		<view class="mid">
+			<view class="midone">手机 :</view>
+			<view class="midone_r"><input placeholder="18927276565" /></view>
+		</view>
+		<view class="br1"></view>
+		<view class="mid">
+			<view class="midone">隐藏手机号 :</view>
+			<view class="midone_TP"><image src="../../static/images/gb.png" style="width: 65rpx;height: 65rpx;"></image></view>
+		</view>
+		<view class="br2"></view>
 		<view class="mid">
 			<view class="midone">邮箱 :</view>
 			<view class="midone_r"><input placeholder="65523355@qq.com" /></view>
 		</view>
+		<view class="br1"></view>
 		<view class="mid">
-			<view class="midone">手机 :</view>
-			<view class="midone_r"><input placeholder="152866565635" /></view>
+			<view class="midone">常用地址 :</view>
+			<view class="midone_r"><input placeholder="上海市松江区亭林镇大叶公路500号" /></view>
 		</view>
-		<view class="mid_two">
-			<view class="midtwo">常住地址 :</view>
+		<view class="br2"></view>
+		<view class="mid">
+			<view class="midone">职业 :</view>
+			<view class="midone_r"><input placeholder="教师" /></view>
+		</view>
+		<view class="br1"></view>
+		<view class="mid">
+			<view class="midone">职称 :</view>
+			<view class="midone_r"><input placeholder="无" /></view>
+		</view>
+		<view class="br1"></view>
+		<view class="mid">
+			<view class="midone">学历 :</view>
+			<view class="midone_r"><input placeholder="本科" /></view>
+		</view>
+		<view class="br3"></view>
+		<!-- <view class="mid_two">
+			<view class="midtwo">常用地址 :</view>
 			<view class="midtwo_r">
 				<textarea placeholder="贵州省贵阳市花溪区XXXXX镇XXXXXX村xxxx组xxxxxxxxx栋xxxxxx层xxxxx" maxlength="100" style="height: 300rpx;width: auto;"></textarea>
 			</view>
-		</view>
+		</view> -->
 		<view class="x">
 		<button class="bc">保存</button>
 		</view>
@@ -39,16 +68,19 @@ export default {
 </script>
 
 <style>
+.all{
+	background-color: #FFFFFF;
+}
 .top {
 	height: 110rpx;
 	/* background-color: #BBBBBB; */
-	border-bottom: 1rpx solid #bbbbbb;
+	border-bottom: 1rpx solid #F1F1F1;
 }
 .topone {
 	/* background-color: #007AFF; */
 	width: 20%;
 	margin-left: 30rpx;
-	margin-top: 50rpx;
+	margin-top: 55rpx;
 	/* border-bottom: 1rpx solid #bbbbbb; */
 }
 .topthree {
@@ -57,9 +89,8 @@ export default {
 	/* background-color:  #FF0000; */
 }
 .image {
-	/* background-color: #4CD964; */
-	margin-left: 570rpx;
-	margin-top: -85rpx;
+	margin-left: 580rpx;
+	margin-top: -67rpx;
 }
 .imag {
 	/* margin-left: 700rpx; */
@@ -67,26 +98,35 @@ export default {
 }
 .mid {
 	/* background-color: #F0AD4E; */
-	height: 85rpx;
-	border-bottom: 1rpx solid #bbbbbb;
+	height: 60rpx;
+	/* border-bottom: 1rpx solid #F1F1F1; */
 }
 .midone {
 	/* background-color: #2C405A; */
 	width: 40%;
-	margin-top: 40rpx;
+	font-size: 30rpx;
+	margin-top: 30rpx;
 	margin-left: 30rpx;
 }
 .midone_r {
 	/* background-color:  #4CD964; */
+	margin-top: -40rpx;
+	text-align: right;
+	margin-right: 50rpx;
+	color: #D2D2D2;
+}
+.midone_r input{
+	font-size: 30rpx;
+}
+.midone_TP{
 	margin-top: -50rpx;
-	text-align: left;
-	margin-left: 150rpx;
-	color: #00BFFF;
+	text-align: right;
+	margin-right: 50rpx;
 }
 .mid_two {
 	/* background-color: #F0AD4E; */
 	height: 280rpx;
-	border-bottom: 1rpx solid #bbbbbb;
+	/* border-bottom: 1rpx solid #bbbbbb; */
 }
 .midtwo {
 	/* background-color: #2C405A; */
@@ -106,5 +146,21 @@ export default {
 		margin-top: 100rpx;
 		width: 500rpx;
 		background-color: #00BFFF;
+	}
+.br1 {
+		width: 100%;
+		height: 3rpx;
+		background-color: #F4F5F6;
+		margin-left: 30rpx;
+	}	
+.br2 {
+		width: 100%;
+		height: 30rpx;
+		background-color: #F5F5F5;
+	}
+.br3 {
+		width: 100%;
+		height: 30rpx;
+		background-color: #F5F5F5;
 	}
 </style>
