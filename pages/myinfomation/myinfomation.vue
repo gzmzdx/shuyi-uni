aca37649054ab2d83298a6a5578aa4abe30263bf
slmeng77 <74227205+slmeng77@users.noreply.github.com>
2020-11-30 20:59:51 +0800

    信息管理，和我的主页

--- pages/myinfomation/myinfomation.vue
+++ pages/myinfomation/myinfomation.vue
@@ -1,49 +1,55 @@
 <template>
 	<view class="all">
-		<view class="top">
+		<form @submit="formSubmit" >
+		<view class="top" >
 			<view class="topone">头像</view>
-			<view><image class="image" src="../../static/images/头像2.jpg" style="width:120rpx;height: 120rpx;border-radius: 100%;":loading="loading" :disabled="disabled" @click="upload" ></image></view>
+			<view><image class="image" :src="head_img" style="width:120rpx;height: 120rpx;border-radius: 100%;":loading="loading" :disabled="disabled" @click="upImg()" ></image></view>
+			<view v-show="false">
+				<input name = "avatar" :value="info.avatar" />
+			</view>
 			<!-- <view class="topthree"><image class="imag" src="../../static/images/更多.png" style="width: 30rpx;height: 35rpx;"></image></view> -->
 		</view>
 		<view class="br2"></view>
 		<view class="mid">
 			<view class="midone">昵称 :</view>
-			<view class="midone_r"><input placeholder="林思鱼" /></view>
+			<view class="midone_r"><input name="nickname" :value="info.nickname" /></view>
 		</view>
 		<view class="br1"></view>
-		<view class="mid">
+		<view class="mid" v-show="isShow">
 			<view class="midone">手机 :</view>
-			<view class="midone_r"><input placeholder="18927276565" /></view>
+			<view class="midone_r"><input name="phoneNum" :value="info.phone" /></view>
 		</view>
 		<view class="br1"></view>
-		<view class="mid">
+		<view class="mid" >
 			<view class="midone">隐藏手机号 :</view>
-			<view class="midone_TP"><image src="../../static/images/gb.png" style="width: 65rpx;height: 65rpx;"></image></view>
+			<view class="midone_TP">
+				<switch checked @change="switchChange" />
+			</view>
 		</view>
 		<view class="br2"></view>
 		<view class="mid">
 			<view class="midone">邮箱 :</view>
-			<view class="midone_r"><input placeholder="65523355@qq.com" /></view>
+			<view class="midone_r"><input name = "email" :value="info.email" /></view>
 		</view>
 		<view class="br1"></view>
 		<view class="mid">
 			<view class="midone">常用地址 :</view>
-			<view class="midone_r"><input placeholder="上海市松江区亭林镇大叶公路500号" /></view>
+			<view class="midone_r" v-text="info.detail_address"></view>
 		</view>
 		<view class="br2"></view>
 		<view class="mid">
 			<view class="midone">职业 :</view>
-			<view class="midone_r"><input placeholder="教师" /></view>
+			<view class="midone_r"><input name = "occupation" :value="info.occupation" /></view>
 		</view>
 		<view class="br1"></view>
 		<view class="mid">
 			<view class="midone">职称 :</view>
-			<view class="midone_r"><input placeholder="无" /></view>
+			<view class="midone_r"><input name="professional" :value="info.professional" /></view>
 		</view>
 		<view class="br1"></view>
 		<view class="mid">
 			<view class="midone">学历 :</view>
-			<view class="midone_r"><input placeholder="本科" /></view>
+			<view class="midone_r"><input name="education" :value="education" /></view>
 		</view>
 		<view class="br3"></view>
 		<!-- <view class="mid_two">
@@ -53,57 +59,171 @@
 			</view>
 		</view> -->
 		<view class="x">
-		<button class="bc">保存</button>
+			<button form-type="submit" class="bc">保存</button>
 		</view>
+		</form>
 	</view>
 </template>
 
 <script>
 	var _self;
 	export default {
-	 data:{
-	  percent:0,
-	  loading:false,
-	  disabled:false
-	 },
+		data() {
+			return {
+				percent:0,
+				loading:false,
+				disabled:false,
+				isShow:true,//是否隐藏电话号码
+				openId:1 ,//暂时是1
+				info:{},//读者信息
+				token:'token',
+				head_img:'',//头像
+				info:{
+					openId:1
+				}
+				
+			};
+		},
+		onLoad() {
+			this.getReaderInfo();
+		},
 	 methods : {
-	  upload : function(){
-	   _self = this;
-	   uni.chooseImage({
-	    count: 1,
-	    sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
-	    sourceType: ['album'], //从相册选择
-	    success: function (res) {
-	     const tempFilePaths = res.tempFilePaths;
-	     const uploadTask = uni.uploadFile({
-	      url : "https://jxsbbx.gzmu.edu.cn/schoolcloud/",
-	      filePath: tempFilePaths[0],
-	      name: 'file',
-		  // fileType: 'image',
-	      formData: {
-	       'user': 'test'
-	      },
-	      success: function (uploadFileRes) {
-	       console.log(uploadFileRes.data);
-	      }
-	     });
+		 //获取读者信息
+		 getReaderInfo(){
+		 	var that = this;
+		 	uni.request({
+		 		 // url:getApp().globalData.URL +'api/reader/app?id='+that.openId,
+		 		 url:getApp().globalData.URL +'api/reader/app',
+		 		 data:{
+		 			 id:that.openId,
+		 			 token:that.token,
+		 		 },
+		 		 header: {
+		 		     'content-type': 'application/json;charset=UTF-8' // 默认值
+		 		   },
+		 		 method:'GET',
+		 		 success:res =>{
+		 			that.info=res.data;
+					that.head_img = res.data.avatar
+		 			}
+		 	})
+		 },
+		 //是否隐藏电话号码
+		 switchChange: function (e) {
+			 this.isShow = e.target.value;
+		 },
+		 formSubmit:function(e){
+			var that = this
+			//let tmp = JSON.stringify(e.detail.value)
+			let tmp = JSON.stringify(e.detail.value)
+			console.log('form发生了submit事件，携带数据为：' + JSON.stringify(e.detail.value))
+			var formdata = e.detail.value
+			// 数据校验
+			// console.log(formdata.birth);
+			//正则表达式
+			let reg_tel = /(^$)|(\d{11})|^((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1}))$/
+			let reg_email = /(^$)|^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/ 
+			//结果标记
+			let sign_tel = RegExp(reg_tel).test(formdata.phoneNum);	
+			let sign_email = RegExp(reg_email).test(formdata.email);
+			// console.log(sign+"，数据类型："+typeof(sign));
+			if(!sign_tel){
+				this.showModal("请检查电话号码格式");
+			}else if(!sign_email){
+				this.showModal('请检查电子邮箱格式')
+			}else{
+				uni.request({
+					//url: getApp().globalData.URL +'api/reader/app?headImg='+that.head_img+'&tmp',
+					url:getApp().globalData.URL +'api/reader/app?openId='+that.openId,
+					//url: getApp().globalData.URL +'api/reader/app',
+					data: tmp,
+					method: "PUT",
+					success:(res)=> {
+						console.log("回调函数信息：",res)
+						this.showModal("更改信息成功了！")
+					},
+					fail:(res)=>{
+						this.showModal("更改信息失败了！")
+					}
+				})
+			}
+			
+		},
+		 //上传图片
+		 upImg(){
+		 	var that = this;
+		 	uni.chooseImage({
+		 	    success: (chooseImageRes) => {
+		 	        const tempFilePaths = chooseImageRes.tempFilePaths;
+		 	        uni.uploadFile({
+		 	            url: getApp().globalData.URL +'api/imgUpload/upload', //仅为示例，非真实的接口地址
+		 	            filePath: tempFilePaths[0],
+		 	            name: 'multipartFile',
+		 	            formData: {
+		 	                'user': 'test'
+		 	            },
+		 	            success: (uploadFileRes) => {
+		 					var datas=JSON.parse(uploadFileRes.data);
+		 					if(datas.errno==0){
+								uni.showToast({
+									title: '更改头像成功！',
+									icon: 'none'
+								});
+		 						that.head_img = getApp().globalData.URL+"upload/图片/" + datas.data[0];
+		 					}else{
+		 						uni.showToast({
+		 							title: '图片上传失败了！',
+		 							icon: 'none'
+		 						});
+		 						}
+		 						console.log("看看赋值是否成功",that.head_img)
+		 					}
+		 	        });
+		 	    }
+		 	});
+		 },
+		 //提示框
+		 showModal(val){
+			 uni.showToast({
+			 	title: val,
+			 	icon: 'none'
+			 });
+		 },
+			 
+	 //  upload : function(){
+	 //   _self = this;
+	 //   uni.chooseImage({
+	 //    count: 1,
+	 //    sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
+	 //    sourceType: ['album'], //从相册选择
+	 //    success: function (res) {
+	 //     const tempFilePaths = res.tempFilePaths;
+	 //     const uploadTask = uni.uploadFile({
+	 //      url : "https://jxsbbx.gzmu.edu.cn/schoolcloud/",
+	 //      filePath: tempFilePaths[0],
+	 //      name: 'file',
+		//   // fileType: 'image',
+	 //      formData: {
+	 //       'user': 'test'
+	 //      },
+	 //      success: function (uploadFileRes) {
+	 //       console.log(uploadFileRes.data);
+	 //      }
+	 //     });
 	 
-	     uploadTask.onProgressUpdate(function (res) {
-	      _self.percent = res.progress;
-	      console.log('上传进度' + res.progress);
-	      console.log('已经上传的数据长度' + res.totalBytesSent);
-	      console.log('预期需要上传的数据总长度' + res.totalBytesExpectedToSend);
-	     });
-	    },
-	    error : function(e){
-	     console.log(e);
-	    }
-	   });
-	  }
+	 //     uploadTask.onProgressUpdate(function (res) {
+	 //      _self.percent = res.progress;
+	 //      console.log('上传进度' + res.progress);
+	 //      console.log('已经上传的数据长度' + res.totalBytesSent);
+	 //      console.log('预期需要上传的数据总长度' + res.totalBytesExpectedToSend);
+	 //     });
+	 //    },
+	 //    error : function(e){
+	 //     console.log(e);
+	 //    }
+	 //   });
+	 //  }
 	 },
-	    onLoad:function(){
-	  
-	    }
 	}
 </script>
 
