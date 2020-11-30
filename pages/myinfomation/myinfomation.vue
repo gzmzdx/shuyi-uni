665923ef1dd6444cb837cef909a5f9051e2d20f9
slmeng77 <74227205+slmeng77@users.noreply.github.com>
2020-11-30 23:13:39 +0800

    信息管理页和我的主页完成！

--- pages/myinfomation/myinfomation.vue
+++ pages/myinfomation/myinfomation.vue
@@ -4,10 +4,6 @@
 		<view class="top" >
 			<view class="topone">头像</view>
 			<view><image class="image" :src="head_img" style="width:120rpx;height: 120rpx;border-radius: 100%;":loading="loading" :disabled="disabled" @click="upImg()" ></image></view>
-			<view v-show="false">
-				<input name = "avatar" :value="info.avatar" />
-			</view>
-			<!-- <view class="topthree"><image class="imag" src="../../static/images/更多.png" style="width: 30rpx;height: 35rpx;"></image></view> -->
 		</view>
 		<view class="br2"></view>
 		<view class="mid">
@@ -49,15 +45,9 @@
 		<view class="br1"></view>
 		<view class="mid">
 			<view class="midone">学历 :</view>
-			<view class="midone_r"><input name="education" :value="education" /></view>
+			<view class="midone_r"><input name="education" :value="info.education" /></view>
 		</view>
 		<view class="br3"></view>
-		<!-- <view class="mid_two">
-			<view class="midtwo">常用地址 :</view>
-			<view class="midtwo_r">
-				<textarea placeholder="贵州省贵阳市花溪区XXXXX镇XXXXXX村xxxx组xxxxxxxxx栋xxxxxx层xxxxx" maxlength="100" style="height: 300rpx;width: auto;"></textarea>
-			</view>
-		</view> -->
 		<view class="x">
 			<button form-type="submit" class="bc">保存</button>
 		</view>
@@ -114,36 +104,34 @@
 		 },
 		 formSubmit:function(e){
 			var that = this
-			//let tmp = JSON.stringify(e.detail.value)
 			let tmp = JSON.stringify(e.detail.value)
 			console.log('form发生了submit事件，携带数据为：' + JSON.stringify(e.detail.value))
 			var formdata = e.detail.value
 			// 数据校验
-			// console.log(formdata.birth);
-			//正则表达式
 			let reg_tel = /(^$)|(\d{11})|^((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1}))$/
 			let reg_email = /(^$)|^\w+((.\w+)|(-\w+))@[A-Za-z0-9]+((.|-)[A-Za-z0-9]+).[A-Za-z0-9]+$/ 
 			//结果标记
 			let sign_tel = RegExp(reg_tel).test(formdata.phoneNum);	
 			let sign_email = RegExp(reg_email).test(formdata.email);
-			// console.log(sign+"，数据类型："+typeof(sign));
 			if(!sign_tel){
 				this.showModal("请检查电话号码格式");
 			}else if(!sign_email){
 				this.showModal('请检查电子邮箱格式')
 			}else{
 				uni.request({
-					//url: getApp().globalData.URL +'api/reader/app?headImg='+that.head_img+'&tmp',
-					url:getApp().globalData.URL +'api/reader/app?openId='+that.openId,
-					//url: getApp().globalData.URL +'api/reader/app',
+					url:getApp().globalData.URL +'api/reader/app?openId='+that.openId+'&headImg='+that.head_img,
 					data: tmp,
 					method: "PUT",
 					success:(res)=> {
-						console.log("回调函数信息：",res)
-						this.showModal("更改信息成功了！")
+						console.log('idhjeiofhiuhfi',res)
+						if(res.statusCode==200){
+							this.showModal("更改信息成功了！")
+						}else{
+							this.showModal("更改信息失败了！")
+						}
 					},
 					fail:(res)=>{
-						this.showModal("更改信息失败了！")
+						this.showModal("网络错误！")
 					}
 				})
 			}
@@ -165,19 +153,15 @@
 		 	            success: (uploadFileRes) => {
 		 					var datas=JSON.parse(uploadFileRes.data);
 		 					if(datas.errno==0){
-								uni.showToast({
-									title: '更改头像成功！',
-									icon: 'none'
-								});
+								this.showModal("头像已上传！")
 		 						that.head_img = getApp().globalData.URL+"upload/图片/" + datas.data[0];
 		 					}else{
-		 						uni.showToast({
-		 							title: '图片上传失败了！',
-		 							icon: 'none'
-		 						});
+								this.showModal("图片上传失败了！")
 		 						}
-		 						console.log("看看赋值是否成功",that.head_img)
-		 					}
+		 					},
+						fail:(res)=>{
+							this.showModal("网络错误！")
+						}
 		 	        });
 		 	    }
 		 	});
@@ -189,40 +173,6 @@
 			 	icon: 'none'
 			 });
 		 },
-			 
-	 //  upload : function(){
-	 //   _self = this;
-	 //   uni.chooseImage({
-	 //    count: 1,
-	 //    sizeType: ['original', 'compressed'], //可以指定是原图还是压缩图，默认二者都有
-	 //    sourceType: ['album'], //从相册选择
-	 //    success: function (res) {
-	 //     const tempFilePaths = res.tempFilePaths;
-	 //     const uploadTask = uni.uploadFile({
-	 //      url : "https://jxsbbx.gzmu.edu.cn/schoolcloud/",
-	 //      filePath: tempFilePaths[0],
-	 //      name: 'file',
-		//   // fileType: 'image',
-	 //      formData: {
-	 //       'user': 'test'
-	 //      },
-	 //      success: function (uploadFileRes) {
-	 //       console.log(uploadFileRes.data);
-	 //      }
-	 //     });
-	 
-	 //     uploadTask.onProgressUpdate(function (res) {
-	 //      _self.percent = res.progress;
-	 //      console.log('上传进度' + res.progress);
-	 //      console.log('已经上传的数据长度' + res.totalBytesSent);
-	 //      console.log('预期需要上传的数据总长度' + res.totalBytesExpectedToSend);
-	 //     });
-	 //    },
-	 //    error : function(e){
-	 //     console.log(e);
-	 //    }
-	 //   });
-	 //  }
 	 },
 	}
 </script>
